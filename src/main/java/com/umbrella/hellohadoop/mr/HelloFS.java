b8fd562518c0250e2157056de63c56a316d49ac2
xudazhou <shin.12@163.com>
2017-04-13 11:28:38 +0800

    distributedcache demo

--- src/main/java/com/umbrella/hellohadoop/mr/HelloFS.java
+++ src/main/java/com/umbrella/hellohadoop/mr/HelloFS.java
@@ -2,9 +2,7 @@ package com.umbrella.hellohadoop.mr;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.slf4j.Logger;
@@ -33,12 +31,27 @@ public class HelloFS extends Configured implements Tool {
         Configuration cfg = this.getConf();
 
         FileSystem fs = FileSystem.get(cfg);
-        FSDataOutputStream fdos = fs.create(new Path("/user/hector/file1.txt"));
-        fdos.close();
 
-        log.info("createNewFile={}", fs.createNewFile(new Path("/user/hector/file2.txt")));
+        /*
+          * -rw-r--r--   1 land supergroup          0 2017-04-12 15:44 /user/hector/file1.txt
+          * 如果文件已经存在，仍然能创建，且会覆盖原文件，更新时间戳
+          * -rw-r--r--   1 land supergroup          0 2017-04-12 15:45 /user/hector/file1.txt
+          */
 
-        log.info("mkdir={}", fs.mkdirs(new Path("/user/hector/dir1")));
+//        FSDataOutputStream fdos = fs.create(new Path("/user/hector/file1.txt"));
+//        fdos.close();
+
+        /*
+         * -rw-r--r--   1 land supergroup          0 2017-04-12 15:44 /user/hector/file2.txt
+         * 如果文件已经存在，则返回失败
+         */
+//        log.info("createNewFile={}", fs.createNewFile(new Path("/user/hector/file2.txt")));
+
+        /*
+         * drwxr-xr-x   - land supergroup          0 2017-04-12 15:44 /user/hector/dir1
+         * 如果目录已经存在，仍然返回成功，应该没有任何变化
+         */
+//        log.info("mkdir={}", fs.mkdirs(new Path("/user/hector/dir1")));
 
         fs.close();
 
--- src/main/java/com/umbrella/hellohadoop/mr/HelloMr.java
+++ src/main/java/com/umbrella/hellohadoop/mr/HelloMr.java
@@ -2,11 +2,23 @@ package com.umbrella.hellohadoop.mr;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.StringTokenizer;
 
 /**
  * Created by xudazhou on 2017/3/20.
@@ -28,9 +40,50 @@ public class HelloMr extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws Exception {
-        JobConf jobConf = new JobConf(this.getConf(), HelloMr.class);
-        jobConf.setMemoryForMapTask(200);
-//        jobConf.setMaxPhysicalMemoryForTask(100);
+        Configuration cfg = this.getConf();
+
+        DistributedCache.addCacheFile(new URI("/user/hector/file1.txt"), cfg);
+
+        Path inpath = new Path(args[0]);
+        Path outputpath = new Path(args[1]);
+
+        FileSystem fs = FileSystem.get(cfg);
+
+        if (fs.exists(outputpath)) {
+            fs.delete(outputpath, true);
+        }
+        fs.close();
+
+        Job job = Job.getInstance(cfg, "hellomr");
+        job.setJarByClass(HelloMr.class);
+        job.setNumReduceTasks(0);
+
+        job.setMapperClass(HelloMapper.class);
+
+        FileInputFormat.addInputPath(job, inpath);
+        FileOutputFormat.setOutputPath(job, outputpath);
+
+        job.waitForCompletion(true);
         return 0;
+    }
+
+    static class HelloMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            Path[] paths = DistributedCache.getLocalCacheFiles(context.getConfiguration());
+            for (Path p : paths) {
+                log.info("p={}", p);
+            }
+        }
+
+        @Override
+        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer st = new StringTokenizer(value.toString());
+            while (st.hasMoreTokens()) {
+                context.write(new Text(st.nextToken()), new IntWritable(1));
+            }
+        }
+
     }
 }
