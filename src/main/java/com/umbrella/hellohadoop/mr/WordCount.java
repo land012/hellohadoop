918be87e9bd91a7bd5c5b035647c04113bea0891
xudazhou <shin.12@163.com>
2017-07-14 20:17:00 +0800

    multi output

--- src/main/java/com/umbrella/hellohadoop/mr/WordCount.java
+++ src/main/java/com/umbrella/hellohadoop/mr/WordCount.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.compress.GzipCodec;
@@ -13,6 +12,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -26,6 +26,7 @@ import java.util.StringTokenizer;
 /**
  * Created by xudazhou on 2017/4/18.
  * SequenceFile
+ * GzipCodec
  */
 public class WordCount extends Configured implements Tool {
 
@@ -47,6 +48,8 @@ public class WordCount extends Configured implements Tool {
 
         Path inputpath = new Path(args[0]);
         Path outputpath = new Path(args[1]);
+        conf.set("output1", args[2]);
+        conf.set("output2", args[3]);
 
         FileSystem fs = FileSystem.get(conf);
         if (fs.exists(outputpath)) {
@@ -92,6 +95,19 @@ public class WordCount extends Configured implements Tool {
     }
 
     static class WCReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        private MultipleOutputs mos;
+        String output1;
+        String output2;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            mos = new MultipleOutputs(context);
+            Configuration cfg = context.getConfiguration();
+            output1 = cfg.get("output1");
+            output2 = cfg.get("output2");
+        }
+
         @Override
         protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
@@ -99,6 +115,9 @@ public class WordCount extends Configured implements Tool {
                 sum += i.get();
             }
             context.write(key, new IntWritable(sum));
+
+            mos.write("wc1", key, new IntWritable(sum), output1);
+            mos.write("wc2", key, new IntWritable(sum*2), output2);
 
 //            String v = String.valueOf(sum);
 //            context.write(key, new BytesWritable(v.getBytes("utf-8")));
